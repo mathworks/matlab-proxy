633c1342d8ae3aa0866505c899f3fbb1f0fdb3a8
Prabhakar Kumar <prabhakk@mathworks.com>
2023-12-19 19:25:09 +0530

    Update to v0.10.1

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ long_description = (HERE / "README.md").read_text()
 
 setuptools.setup(
     name="matlab-proxy",
-    version="0.10.0",
+    version="0.10.1",
     url=config["doc_url"],
     author="The MathWorks, Inc.",
     author_email="cloud@mathworks.com",
