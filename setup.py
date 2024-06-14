f7d9142fe570c40c5e8eb0c224ba9c22d6aa191f
Prabhakar Kumar <prabhakk@mathworks.com>
2024-06-14 12:18:01 +0530

    Update to v0.18.2

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ long_description = (HERE / "README.md").read_text()
 
 setuptools.setup(
     name="matlab-proxy",
-    version="0.18.1",
+    version="0.18.2",
     url=config["doc_url"],
     author="The MathWorks, Inc.",
     author_email="cloud@mathworks.com",
