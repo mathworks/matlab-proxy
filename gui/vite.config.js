fe03e10fb4bdfeecde81f126906003406161efe9
Sourabh Kondapaka <skondapa@mathworks.com>
2025-03-19 10:54:22 +0000

    Upgrades vite, fixes GitHub advisory: GHSA-67mh-4wv8-2f99.

--- gui/vite.config.js
+++ gui/vite.config.js
@@ -41,8 +41,16 @@ export default defineConfig({
     },
     server: {
         proxy: {
-            '/': {
-                target: 'http://localhost:8888',
+            // Need to specify all API endpoints that matlab-proxy supports. 
+            // Static files will be served by vite dev server whereas the rest of the requests will be proxied to the matlab-proxy server.
+            
+            // Approach 1: Explicitly state all REST endpoints that matlab-proxy handles. This is harder to maintain as there maybe frequent changes to matlab-proxy endpoints
+            // '^/(get_status|authenticate|get_auth_token|get_env_config|start_matlab|clear_client_id|stop_matlab|set_licensing_info|update_entitlement|shutdown_integration)': {
+            
+            // Approach 2: Proxy anything which doesn't match the specified path below. This is easier to maintain as proxy everthing other than static content
+            '^(?!/favicon\.ico|/index\.html|/manifest\.json|/robots\.txt|/static/|/index\.css|/src/|/@vite/client|/node_modules/|/@react-refresh).*': {
+                // URL at which matlab-proxy server is running at
+                target: 'http://localhost:8889',
                 changeOrigin: true, 
             }
         }
